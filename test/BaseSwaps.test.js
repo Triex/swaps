04ba1c0185532b790c40707d955e4dd687f574de
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2019-03-22 16:55:20 +0300

    add more tests

--- test/BaseSwaps.test.js
+++ test/BaseSwaps.test.js
@@ -9,11 +9,14 @@ const {
   time: { duration }
 } = require("openzeppelin-test-helpers");
 
+const { expect } = require("chai");
+
 const Swaps = artifacts.require("BaseSwaps");
 const Token = artifacts.require("TestToken");
 
 contract("BaseSwaps", ([owner, ...accounts]) => {
   let now;
+  let gasPrice = new BN("20000000000");
 
   beforeEach(async () => {
     now = await time.latest();
@@ -135,4 +138,141 @@ contract("BaseSwaps", ([owner, ...accounts]) => {
     const from = accounts[0];
     await shouldFail(swaps.depositQuote(token.address, { from }));
   });
+
+  it("successful swap", async () => {
+    const ethLimit = ether("1");
+    const tokenLimit = ether("2");
+    const token = await Token.new();
+    const swaps = await Swaps.new(
+      owner,
+      ZERO_ADDRESS,
+      token.address,
+      ethLimit,
+      tokenLimit,
+      now.add(duration.minutes(1))
+    );
+
+    await swaps.sendTransaction({ value: ethLimit, from: accounts[0] });
+
+    await token.mint(accounts[1], tokenLimit);
+    await token.approve(swaps.address, tokenLimit, { from: accounts[1] });
+    const balanceTracker = await balance.tracker(accounts[1]);
+    const { receipt: { gasUsed }, logs } = await swaps.depositQuote({ from: accounts[1] });
+
+    expectEvent.inLogs(logs, "Swap", { byUser: accounts[1] });
+
+    expect(await token.balanceOf(accounts[0])).to.be.bignumber.equal(tokenLimit);
+    expect(await token.balanceOf(accounts[1])).to.be.bignumber.equal(new BN("0"));
+
+    expect(await balanceTracker.delta()).to.be.bignumber.equal(ethLimit.sub(new BN(gasUsed).mul(gasPrice)));
+  });
+
+  it("swap between tokens", async () => {
+    const tokens = [await Token.new(), await Token.new()];
+    const limits = [ether("1"), ether("2")];
+
+    const swaps = await Swaps.new(
+      owner,
+      tokens[0].address,
+      tokens[1].address,
+      limits[0],
+      limits[1],
+      now.add(duration.minutes(1))
+    );
+
+    for (let i = 0; i < tokens.length; i++) {
+      await tokens[i].mint(accounts[i], limits[i]);
+      await tokens[i].approve(swaps.address, limits[i], { from: accounts[i] });
+    }
+
+    await swaps.depositBase({ from: accounts[0] });
+    await swaps.depositQuote({ from: accounts[1] });
+
+    expect(await tokens[0].balanceOf(accounts[0])).to.be.bignumber.equal(new BN("0"));
+    expect(await tokens[0].balanceOf(accounts[1])).to.be.bignumber.equal(limits[0]);
+    expect(await tokens[1].balanceOf(accounts[1])).to.be.bignumber.equal(new BN("0"));
+    expect(await tokens[1].balanceOf(accounts[0])).to.be.bignumber.equal(limits[1]);
+  });
+
+  it("swap between many addresses", async () => {
+    const baseInvestors = accounts.slice(0, accounts.length / 2);
+    const quoteInvestors = accounts.slice(accounts.length / 2, accounts.length);
+
+    const baseToken = await Token.new();
+    const quoteToken = await Token.new();
+
+    const baseLimit = ether("1");
+    const quoteLimit = ether("2");
+
+    const swaps = await Swaps.new(
+      owner,
+      baseToken.address,
+      quoteToken.address,
+      baseLimit,
+      quoteLimit,
+      now.add(duration.minutes(1))
+    );
+
+    const baseAmount = baseLimit.div(new BN(baseInvestors.length));
+    for (let i = 0; i < baseInvestors.length; i++) {
+      await baseToken.mint(baseInvestors[i], baseAmount);
+      await baseToken.approve(swaps.address, baseAmount, { from: baseInvestors[i] });
+      await swaps.depositBase({ from: baseInvestors[i] });
+    }
+
+    const quoteAmount = quoteLimit.div(new BN(quoteInvestors.length));
+    for (let i = 0; i < quoteInvestors.length; i++) {
+      await quoteToken.mint(quoteInvestors[i], quoteAmount);
+      await quoteToken.approve(swaps.address, quoteAmount, { from: quoteInvestors[i] });
+      await swaps.depositQuote({ from: quoteInvestors[i] });
+    }
+
+    expect(await swaps.isSwapped()).to.be.equal(true);
+    expect(await swaps.baseInvestors()).to.be.eql(baseInvestors);
+    expect(await swaps.quoteInvestors()).to.be.eql(quoteInvestors);
+
+    const quoteAmountForBase = baseAmount.div(baseLimit).mul(quoteAmount);
+    for (let i = 0; i < baseInvestors.length; i++) {
+      expect(await baseToken.balanceOf(baseInvestors[i])).to.be.bignumber.equal(quoteAmountForBase);
+    }
+
+    const baseAmountForQuote = quoteAmount.div(quoteLimit).mul(baseAmount);
+    for (let i = 0; i < quoteInvestors.length; i++) {
+      expect(await quoteToken.balanceOf(quoteInvestors[i])).to.be.bignumber.equal(baseAmountForQuote);
+    }
+  });
+
+  it("deposit from one address to both sides", async () => {
+    const baseToken = await Token.new();
+    const quoteToken = await Token.new();
+
+    const baseLimit = ether("1");
+    const quoteLimit = ether("2");
+
+    const swaps = await Swaps.new(
+      owner,
+      baseToken.address,
+      quoteToken.address,
+      baseLimit,
+      quoteLimit,
+      now.add(duration.minutes(1))
+    );
+
+    await baseToken.mint(accounts[0], baseLimit);
+    await baseToken.approve(swaps.address, baseLimit, { from: accounts[0] });
+    await swaps.depositBase({ from: accounts[0] });
+
+    await quoteToken.mint(accounts[0], quoteLimit);
+    await quoteToken.approve(swaps.address, quoteLimit, { from: accounts[0] });
+    await swaps.depositQuote({ from: accounts[0] });
+
+    expect(await swaps.isSwapped()).to.be.equal(true);
+    expect(await baseToken.balanceOf(accounts[0])).to.be.bignumber.equal(baseLimit);
+    expect(await quoteToken.balanceOf(accounts[0])).to.be.bignumber.equal(quoteLimit);
+  });
+
+  // todo: refund before filled one side
+  // todo: refund after one side filled
+  // todo: try to refund after swap
+  //
 });
